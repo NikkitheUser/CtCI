67d35db41010e708f795371447e2b685ae76ee2f
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:31:08 -0700

    Create 6_1.cpp

---
+++ ch6/6_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//the heavy pill 6.1
+int main() {
+  cout<<"we have 20 pill bottels"<<endl;
+  int weight=(20*21)/2;
+
+  int done=(weight-210)/0.1;
+  cout<<done<<endl;
+}
