e5a42bb528430d32ba90ea0890bf16180d22bb4d
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:38:48 -0700

    Create 11_4.cpp

---
+++ ch11/11_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*
+no test tools:
+
+without using testing tools, we can test a webpage in various ways:
+-response time, we want to test the webpage on multiple devices. this allows us to see if, for example, the desktop version works fine but theres an issue with the mobile page
+-user acess, besides the people who worked on the webpage, can others zacess it? if the page was made in califronia, is there any issues if someone from maryland loads it?
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
