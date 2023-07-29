3250a2cb3c132b70f3f6e05c00fb84686af8f4f4
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:54:23 -0700

    Create 14_4.cpp

---
+++ ch14/14_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+Joins
+ INNER JOIN: returns rows when there is a match in both tables.
+
+LEFT JOIN: returns all rows from the left table, even if there are no matches in the right table.
+
+RIGHT JOIN: returns all rows from the right table, even if there are no matches in the left table.
+
+FULL JOIN: combines the results of both left and right outer joins.
+
+SELF JOIN: joins a table to itself as if the table were two tables, temporarily renaming at least one table in the SQL statement.
+
+CARTESIAN JOIN: returns the Cartesian product of the sets of records from the two or more joined tables.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
