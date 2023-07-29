fdcc0c13d7ff9c624b7dc5a20270149999c0cc3c
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:27:20 -0700

    Create 9_6.cpp

---
+++ ch9/9_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/*
+Sales Rank:
+this sounds similar to how amazon works. 
+lets assume we need to factor in sales as in how many times an item has been purchased, not by the ranking of customers. this will save us the headache of making an equation for averaging out the amount of ranks.
+we are gonna assume that the database can be updated each week. theres a lot of items that are being sold, it would be to much to constantly update the ranking of each item for each catagory.
+
+we can make a simple system to start out
+purchase system -> that sends the orders to the database -> that then sorts the data and sends it to a sales rank data-> that then gets sent to the web page, showing users the data
+
+
+  */
+int main() {
+  std::cout << "if you want explinations, read the commented out portion.";
+}
