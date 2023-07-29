0b8cf891b18946df17f7cf739775599f1eee498c
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:34:46 -0700

    Create 6_10.cpp

---
+++ ch6/6_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+a binary indicator for poisoned or unpoisoned. Is it possible to map 1000 keys to 10 binary values such that each key is mapped to a unique configuration of values? Yes, of course. This is what a binary number is.
+We can take each bottle number and look at its binary representation. If there's a 1 in the ith digit, then we will add a drop of this bottle's contents to test strip i. Observe that 210 is 1024, so 10 test strips will be enough to handle up to 1024 bottles.
+We wait seven days, and then read the results. If test strip i is positive, then set bit i of the result value. Reading all the test strips will give us the ID of the poisoned bottle.
+
+int findPoisonedBottle(ArrayList<Bottle> bottles, ArrayList<TestStrip> strips) 
+{
+void runTests(ArrayList<Bottle> bottles, ArrayList<TestStrip> testStrips) 
+{
+ArrayList<Integer> getPositiveOnDay(ArrayList<TestStrip> testStrips, int day) 
+{
+int setBits(ArrayList<Integer> positive) {
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
