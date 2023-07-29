ef301f5620fd2331d24e38e2d545071aead1bdae
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:31:38 -0700

    Create 10_6.cpp

---
+++ ch10/10_6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/*
+Sort Big file:
+
+Lets try a externalsort method, starting with dividing the file into x megabytes. 
+x memory is what we have available.
+each divided file is then sorted and saved back into our file system.
+once its all sorted, we can merge everything back together, now we have a fully sorted file. 
+*/
+int main() {
+  std::cout << "the answer is in the commment";
+}
