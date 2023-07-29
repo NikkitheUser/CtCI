d0e36884bff331a85e21a7a337b8f118fe46cd14
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:57:11 -0700

    Create 15_1.cpp

---
+++ ch15/15_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/*
+thread vs process
+process: like an instance in a program. its an independent entity where memory and CPU time is located.
+thread: this exisits in a process. it modifies the resources of a process.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
