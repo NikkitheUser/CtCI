8928f8ff7bce6eb90cd4c2bee46ae55843da7c5d
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:32:34 -0700

    Create 6_5.cpp

---
+++ ch6/6_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+how to get to 4 quarts of water with two jugs that hold 5 and 3 quarts. they can not hold half, the jugs cannot hold half water. we need one jug to hold 4, not one holding 4 and the other 1. 
+
+5 quart  3 quart
+5         0
+2         3
+2         0
+0         2
+5          2
+4          3
+4
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
