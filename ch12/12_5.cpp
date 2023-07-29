fa4715f4b38625e53d4eb0bdfd7e4d7e8e266a76
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:44:04 -0700

    Create 12_5.cpp

---
+++ ch12/12_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+shallow vs deep copy
+deep copies can copy all member alues from one object to another, along with pointers.
+shallow copies can do this too, but it cant work with pointers.
+
+we use shallow copies to quickly move objects around without actually needing to do complicated duplications.
+deep copies are used for more complex movements, and are used more often than shallow copies.
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
