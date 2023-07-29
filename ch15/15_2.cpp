7eda120d83b478b4fd83b4b24a56a9b66ca92e08
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:57:25 -0700

    Create 15_2.cpp

---
+++ ch15/15_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+context switch:
+
+we can do something similar to a ACK (acknologment) in network security.
+
+When switch1 goes off, it will trigger switch2 and send the time that it triggered. switch2 will send an acknowlgement to switch1, along with the time frame it got the message.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
