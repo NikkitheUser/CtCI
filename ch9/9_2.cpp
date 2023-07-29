4190f1ed14aa668ef3288f0ab595e8ed12b0b2f7
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:24:56 -0700

    Create 9_2.cpp

---
+++ ch9/9_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+Social Network:
+for this problem, im going to shrink the scale to 100 users. we will have to focus on scalability, since we will be getting more than 100 users, but for this question we will keep it light.
+
+for this scale, we can try to implement a breadth-first search, or BFS for short. we use this to figure out the problem " how to design a way to show the shortest path between two people" 
+
+how this works is that itll take the starting user and the destination user into the search. as it does, it will look to see who the start user knows that the destination user also knows. creating a collsion search. 
+
+  */
+int main() {
+  std::cout << "if you want explinations, read the commented out portion.";
+}
