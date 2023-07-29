3051e9f2034fe60029a47e10a624f71d5fbda6ff
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:34:27 -0700

    Create 6_9.cpp

---
+++ ch6/6_9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+ pair n's factors by their complements. For example, if n is 36, the factors are (1, 36), (2, 18), (3, 12), (4, 9), (6,6). Note that (6, 6) only contributes one factor, thus giving n an odd number of factors.
+Question: How many perfect squares are there?
+There are 10 perfect squares. You could count them (1 , 4,9, 16, 25, 36,49,64, 81, 100)' or you could simply realize that you can take the numbers 1 through 10 and square them:
+11, 22, 33, ... , 1010
+Therefore, there are 10 lockers open at the end of this process.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
