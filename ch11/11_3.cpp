17087d12b531b02742123f095f5fb1c94a72ad32
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:38:21 -0700

    Create 11_3.cpp

---
+++ ch11/11_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+chess test:
+with a program like this, we can stress test it to see how it handles improbable issues:
+-no pieces on the board
+-the board is full of pieces
+-we ask it to move a piece to the negatives
+-have either more or less white pieces than black pieces
+- delete a piece while moves
+-use strings instead of ints
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
