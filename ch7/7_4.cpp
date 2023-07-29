f098788abf6466e728505b10789928bff972bc34
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 17:19:04 -0700

    Create 7_4.cpp

---
+++ ch7/7_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//make a parking lot
+
+class lot{
+int spaceNum;//the space number
+bool avail;//is someone using it?
+int row;
+int level;
+};
+int main() {
+cout << "Hello World!\n";
+}
