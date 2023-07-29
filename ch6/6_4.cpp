49deec2a6ce40784b7628dd1528e9b9015838317
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:32:11 -0700

    Create 6_4.cpp

---
+++ ch6/6_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+the onlyway for these ants to not collde if they all walk in the same direction.
+so there are three ants and two directions they can move in a triangle formation
+
+  * 
+ * *
+its 50/50 for an ant to move clockwise.
+so for all three ants to move in the same direction would be
+a proability of (1/2)^3 + (1/2)^3 =1/4
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
