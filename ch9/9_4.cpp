eb10b5eaca7bc8f8d8a9aed55f15c8b7b4329010
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:25:28 -0700

    Create 9_4.cpp

---
+++ ch9/9_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+Duplicate URLS: you have 10 billion urls. how do we detect the duplicate docs? assume duplicate refers to being identical 
+
+this is gonna be large. we can assume that this will take between 3-4 terabytes of data. so we are going to have to power through that fact if we want to figure out the best method to figure out this problem. 
+
+in a simple senario, where the amount of data isnt an issue, we can use disk storage as a solution. 
+store all the data onto one machine, we can pass through the documentation twice. first rounf would be for splitting the urls by hash data. 
+the second round will be to actually find the duplicate data.
+
+  */
+int main() {
+  std::cout << "if you want explinations, read the commented out portion.";
+}
