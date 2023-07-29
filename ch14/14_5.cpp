4dd26ec0fb48ac86335d133699800364613f5e79
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:54:49 -0700

    Create 14_5.cpp

---
+++ ch14/14_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+Denormalization:
+
+demormalization is how we can add redudant data to one or more tables.
+pros:
+-we can retreive data faster due to less joints needed
+-queries to retreve can be simpler due to looking at less tables
+
+cons:
+-data redudancy requires us to use more storage
+-denormalization can make updates and inserts harder to write in
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
