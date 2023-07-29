c66ccbc0445070c25c513627690343a659041413
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:11:07 -0700

    Create 8_11.cpp

---
+++ ch8/8_11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//coins
+//given quaters (25), dimes(10),(nickels(5)),pennies(1), write ways to repsent n cents
+
+int countCents(int value){
+  int count=0;
+
+
+  return count;
+}
+int main() {
+  cout << "let us use 50 as our main number";
+  int value=50;
+  
+}
