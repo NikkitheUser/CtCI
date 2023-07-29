419b32c56b49149c455d3fbb03a996614e02a930
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:50:22 -0700

    Create 13_5.java

---
+++ ch13/13_5.java
@@ -0,0 +1,21 @@
+
+//Hashmap:
+//iterates through keys, tho order is arbitrary. uses linked lists
+
+//treemap:
+//keys are ordered and can be sorted. 
+
+//LinkHashMap: 
+//like a hashmap, with linked lists and pointers pointing to the next pointer
+
+//if we needed to map names for a family tree, we would use a treemap
+
+//if we need to make the ordereing of keys match that of order of inserttion, we use the linkhashmap
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
