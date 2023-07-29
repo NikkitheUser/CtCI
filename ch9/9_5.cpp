c0c6e3d1605d0cc1480fb384c211f80b664e3cff
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:25:47 -0700

    Create 9_5.cpp

---
+++ ch9/9_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+Cache:
+list of assumptions based on this question:
+- the quieres we are gonna cache ais gonna be around the millions
+- calling will be quick
+- besides the processSearch, everything else works as it should without us mentioning it
+
+
+for a cache to work as intended it needs to remove old data to give way to new data and it has to be good at looking up data as well.
+
+we can do this by using a hash table for the look up portion, and mesh it together with a linked list for the purging data portion. 
+
+  */
+int main() {
+  std::cout << "if you want explinations, read the commented out portion.";
+}
