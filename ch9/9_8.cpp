fea7c16c5431a514ae7d816b22c344682aa6e4af
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:28:09 -0700

    Create 9_8.cpp

---
+++ ch9/9_8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/* 
+Pastebin:
+lets make some assumptions:
+-we can delete documents after a time period
+-users shouldnt be able to guess urls to others documents
+- this site will get heavy traffic
+
+lets start off with:
+url to file database->server with files X3
+we search up a url, and we go through the servers that have the files
+
+*/
+int main() {
+  std::cout << "check the comments for the idea";
+}
