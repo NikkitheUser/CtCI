c4d347f47f0f2d4919725fad087cb94f6537dfd0
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:33:49 -0700

    Create 6_7.cpp

---
+++ ch6/6_7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/*
+okay, lets say theres 10 families left.
+they keep having kids until they have a girl.
+theres 50/50 on the childs gender. 
+
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
