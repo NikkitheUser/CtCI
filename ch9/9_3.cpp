fe2c27e0bebecbf1311074e545c9cbefe8413c0d
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:25:15 -0700

    Create 9_3.cpp

---
+++ ch9/9_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+Web Crawler:
+how do we design a way to avoid infinite loops?
+
+in the situation of a web crawler, a infinte loop can occur if a cycle occurs during the process. 
+
+one way to do this is to dectect a cycle, this can be done witha hash table. we use this hashtable as a way to see if we entered a web page or not. 
+
+
+  */
+int main() {
+  std::cout << "if you want explinations, read the commented out portion.";
+}
