f91c5956542b5c8cf280fd8960318168f745a35e
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:27:41 -0700

    Create 9_7.cpp

---
+++ ch9/9_7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*
+Personal Financial Manager:
+so lets say that this will:
+-connect to your bank, and sort through your fincial history
+-allow us to make multiple bank accounts
+-and we can see outgoing and incoming transactions, as well as your current balance
+
+we can build a simple database:
+we synch the bank data-> send it to a categorizer(split between incoming/outgoing money)-> send it through the "finacial manager"-> then it hits the frontend for the user to see
+  */
+int main() {
+  std::cout << "check the comments for the idea";
+}
