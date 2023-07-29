7ed828e823f7bf846d5145c4f581d3245266c5f3
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:33:11 -0700

    Create 6_6.cpp

---
+++ ch6/6_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*
+we are removing all the blue eyed folks. we dont know how many are on the island, but we know there is at least one. the indivudal with blue eyes are unaware they have blue eyes, and no one can tell the others.
+
+first b will be blue eyed folks
+      g will be general population
+      b>0, there is atleast ONE blue eyed person
+
+if there is one person will blue eyes:
+everyone is gathered in the square and can look around. the person with blue eyes can figure out that everyone else has brown eyes, and van determine that oops, they are the one that has to go. takes the first flight
+
+2 folks with blue eyes:
+theres 2 b, but they dont know how many total b there are. lets call them adam and james. adam sees that the next day that james is still on the island, despite having blue eyes. and concludes that he (adam) has blue eyes too. both men leave on the same night, night 2.
+    
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
