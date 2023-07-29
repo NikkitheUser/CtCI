25063df7af7f77aaae84e0468ae630f871bcda43
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:48:57 -0700

    Create 13_2.cpp

---
+++ ch13/13_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/*
+return from Finally":
+
+in this case, yse the block will be executed. if the try block exits then the fianlly block is excuted.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
