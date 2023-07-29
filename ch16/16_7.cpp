6df638c4eebe61b65c63778091eb223c42e573f3
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:37:06 -0700

    Create 16_7.cpp

---
+++ ch16/16_7.cpp
@@ -0,0 +1,17 @@
+#include <limits.h>
+using namespace std;
+int isMax(int a, int b) { 
+    int c = a - b; //we subtract a-b to get out int c
+    int k = (c >> ((sizeof(int) * CHAR_BIT) - 1)) & 0x1; 
+  
+    int max = a - k * c; //max is then equal to our first int minus k*c
+    return max;
+}
+int main() {
+  int num1,num2;
+  cout << "lets find the max! give me a number"<<endl;
+  cin>>num1;
+  cout<<"now another number!"<<endl;
+  cin>>num2;
+cout<<isMax(num1, num2)<<" is the bigger number"<<endl;
+}
