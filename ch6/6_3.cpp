ed1dd29b8d919f82b7c8a74a1ff022cb6e80ac55
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:31:52 -0700

    Create 6_3.cpp

---
+++ ch6/6_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+/*
+try to lay down a domino on row 1, now notice that it has to strech into row 2. and then it streches into row three. we cant lay down all the dominoes, it will end up in the other row if we lay them down on the rows.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
