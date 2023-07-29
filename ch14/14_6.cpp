173ae59ad3dd5e67509604f54f91ea241962f824
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:55:15 -0700

    Create 14_6.cpp

---
+++ ch14/14_6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/*
+entity-Relationship diagram:
+there are better ways for me to do this diagram,but ill be using replit for this
+draw an entitiy-relationship diagram for a database withcompanies, people, and professionals
+
+people would have information about:
+name, Sex, birth, phone, adress
+
+professional would have people info as well as:
+Degree and experience
+
+companies would hold:
+company name and adress of company
+
+professional/people would have a relationship with comapny:
+date of joining, salary
+
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
