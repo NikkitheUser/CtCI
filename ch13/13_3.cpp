cc3f2b0125539da3024f48598be4b8f7e604b50c
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:49:17 -0700

    Create 13_3.cpp

---
+++ ch13/13_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+final,etc:
+
+final: we use this term for control. Final is used for variables, classes or methods that are changable.
+
+finally: this term is used for try/catch methods. its used so a segment of code will always be executed
+
+finalize: written as finalize(); is actually a method that we use when the garbage collector sees that all refrences have been used 
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
