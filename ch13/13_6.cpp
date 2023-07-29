70c0b9e5f1c1037a8d29f955be51c17ac7ffe36e
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:50:42 -0700

    Create 13_6.cpp

---
+++ ch13/13_6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/*
+object reflection:
+this gives us reflective info about classes and objects. this can range from getting a new instance of a class, getting info about methods, and getting/setting object fields directly by getting field reference(racess modifier is not a worry)
+
+this is useful to us since we observe the behavior of our application. Since we can see how it behaves, this makes debuging and testing easier as well.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
