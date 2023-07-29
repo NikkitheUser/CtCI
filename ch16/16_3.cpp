e47585a198fc6aa1a7e0886ab53be8b74d5c8f6c
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 19:36:10 -0700

    Create 16_3.cpp

---
+++ ch16/16_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+we are gonna use a case that has 2 non-parallel and 2 non vertical lines
+
+we can start by figuring out the y intercept and the slopes of the lines
+
+then we can see where they intersect and if they do intersect.
+
+how do we do the first part?
+we can do y=mx+b
+so we can do
+y=4x+1 and y=3x+2 
+with this we can see if the math will give us the intersection when we move the x around.
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
