0b9b5fee18f11963fcd9bc64e3057edf9ac59a25
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:39:13 -0700

    Create 11_5.cpp

---
+++ ch11/11_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*
+how to test a pen:
+who is the pen for?
+what kind of pen is it?
+what type of material is the pen supposed to write on?
+can the pen ink be removed? if so with what?
+does it come in one sigular color or more?
+
+we then try to see how well it does its intedned purpose.
+we write on the material that the pen is supposed to be good at wrtiting on.
+we test each color to see how long it takes for the ink to run out and how legiable it is on the material.
+we then see if the pen can be washed out of the material, and which colors remove easier.
+
+
+  */
+int main() {
+  std::cout << "Hello World!\n";
+}
