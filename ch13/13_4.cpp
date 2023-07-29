6502f9e3ca73743d12f597845106c64320a84af1
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:49:46 -0700

    Create 13_4.cpp

---
+++ ch13/13_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/* 
+generics vs. templates
+c++: 
+-can use privmitive types (int)
+-type parameters can be used
+
+
+java:
+-cant use primitive types
+- cant support parameters being instantiated, but can restict its type
+-cant be used for static methods
+
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
