17794532d5a4d04d108f331e423e3d1185d2563a
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-07-28 18:40:45 -0700

    Create 11_6.cpp

---
+++ ch11/11_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*
+test an atm:
+-besides the "general public", who else is intended to use this? users in wheelchairs, hearing impaired, blind folks?
+-what can the atm do? can it check you account balance, make withdraws, transactions, can it take in your checks?
+
+we can mess with it first digitally.
+-withdraw non-exisitent money
+-ask for a non existent account
+-make an account name that is all integers, all special characters or blank
+
+
+then we test for security:
+-what happens if you fail to get into an account to many times?
+-can the system detect if one account is being acessed at the same time in different locations?
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
