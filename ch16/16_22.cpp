0ae4bd9afdb2f13ba16311620cb35e9ccd607129
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-09 17:14:37 -0700

    Create 16_22.cpp

---
+++ ch16/16_22.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//Langtons ant
+//1. at a white space, flip the color of the square, turn 90 degrees right (clockwise), and move forward one unit
+//2. at a black square, flip the color of the square, turn 90 degrees left (counter-clockwise), and move forward one unit.
+int main() {
+//we can make this similar to the tic tac toe one, we will use "x" and "o" for black and white.
+  //we can make a function "2black" and "2white". the first will change white blocks to black and 2white will do the opposite
+}
