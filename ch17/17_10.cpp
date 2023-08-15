811950760e2085592aec9a7774abda297e4d4d93
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-15 12:10:39 -0700

    Update 17_10.cpp

--- ch17/17_10.cpp
+++ ch17/17_10.cpp
@@ -4,9 +4,9 @@ using namespace std;
 //majority element 
 //baiscally find out what number pops up the most
 int majorityElement(vector<int>& nums) {
-    if (nums.empty()) return -1;
+    if (nums.empty()) return -1;//if we see theres nothing in the vector
 
-    int maj = nums[0];
+    int maj = nums[0];//this is where we keep the current majority number
     int count = 1;
 
     for (int i = 1; i < nums.size(); i++) {
