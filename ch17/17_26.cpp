4e29334c51906056e8e786204c02aebb2424bcd6
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-15 15:10:54 -0700

    Update 17_26.cpp

--- ch17/17_26.cpp
+++ ch17/17_26.cpp
@@ -4,7 +4,19 @@
 #include <assert.h>
 using namespace std;
 //sparse similarity
+//The similarity of two documents (each with distinct words) is defined to be the size of the intersection 
+//divided by the size of the union. For example, if the documents consist of integers, the similarity of {1, 5, 3} and {1, 7, 2, 3} is e. 4, 
+//because the intersection has size 2 and the union has size 5. We have a long list of documents 
+//(with distinct values and each with an associated ID) where the similarity is believed to be "sparse:'That is,
+//any two arbitrarily selected documents are very likely to have similarity O. Design an algorithm that returns a list of pairs of 
+//document IDs and the associated similarity. Print only the pairs with similarity greater than O. Empty documents should not be 
+//printed at all. For simplicity, you may assume each document is represented as an array of distinct integers.
 
+//we just unload the combos, calculate intersections
+//took a while to understand 
+//some of these ideas had to be taken from elsewhere
+//Understood how to find this in normal calculations
+//took a few hours(and a lot of youtube videos) to do calculations in C++
 unordered_set<int> calc_union(const vector<int> &one, const vector<int> &two) {
     unordered_set<int> unioned;
     unioned.reserve(max(one.size(), two.size()));
