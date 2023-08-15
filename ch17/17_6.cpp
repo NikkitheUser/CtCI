0621d6780b6129d917f107a9a5252075134efbed
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-15 15:34:23 -0700

    Update 17_6.cpp

--- ch17/17_6.cpp
+++ ch17/17_6.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+//count of 2s
+//we write a method that can count the number of 2s between 0 and n
+//lets assume it means "2,4,6,8,etc/"
 int numberOf2s(int n){
   int count=0;
   while(n>0){
