7e9fa0f2bb30d1e3577c514b951c1200535f4268
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-15 15:27:30 -0700

    Update 17_3.cpp

--- ch17/17_3.cpp
+++ ch17/17_3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-
+//random set
+// Write a method to randomly generate a set of m integers from an array of size n. 
+//Each element must have equal probability of being chosen.
 int pickMRecursivelt(int original[], int m){
   if(m>original[].length())return 0;
     int subset[]=new int[m];
@@ -20,5 +22,5 @@ for (int i=m;i<original[].length();i++){
 int main() {
   int original[23];
    int m;
-  cout << "Hello World!\n";
+
 }
