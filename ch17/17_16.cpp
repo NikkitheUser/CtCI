5cce10ea01065e7749549857c599e0e2d6d013c3
NikkitheUser <99938903+NikkitheUser@users.noreply.github.com>
2023-08-15 13:32:46 -0700

    Update 17_16.cpp

--- ch17/17_16.cpp
+++ ch17/17_16.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 #include <vector>
 #include <math.h>
-vector<Schedule> masseuse_max_bookings(const vector<int> & appointments) {
-    vector<Schedule> s;
-    s.reserve(appointments.size());
+//the masseuse
+//we have a vector of bookings
+//she needs 15 mins of breaks between each booking
+//none can overlap, non can be removed 
+vector<Schedule> masseuse_max_bookings(const vector<int> & appointments) {//this will take the vector of appointments
+    vector<Schedule> s;//vector=s
+    s.reserve(appointments.size()); 
     int prev = 0;
     for (const int i : appointments) {
         int acc_next = i + prev;
-        s.emplace_back(prev, acc_next + 15);
+        s.emplace_back(prev, acc_next + 15);//we add in the extra 15 mins
         prev = acc_next;
     }
     vector<int> sol1 = interval_scheduling(s);
@@ -22,10 +26,11 @@ vector<Schedule> masseuse_max_bookings(const vector<int> & appointments) {
 int main()
 {
     vector<int> bookings = { 30, 15, 60, 75, 45, 15, 15, 45 };
-    vector<Schedule> sol = masseuse_max_bookings(bookings);
+    vector<Schedule> sol = masseuse_max_bookings(bookings);//new vector for the final booking
     int acc_sum = 0;
     for (auto i : sol) {
         acc_sum += i.weight - 15;
         cout << "(" << i.start << ", " << i.end << ")" << " -> " << acc_sum << endl;
+        //(30,60,45,45)->180
     }
 }
